f2bac68a66a3a12d8106cd62dc69d64811ef9f2c
Dharmesh <dharmeshkota123@gmail.com>
2023-08-26 20:10:53 +0530

    Backend Finished!

--- routes/project.js
+++ routes/project.js
@@ -8,7 +8,7 @@ const projectController = require('../controller/project_controller');
 // Router-Controller cycle
 router.use('/comments', require('./comments')); //redirected to controller rendering comments page
 router.post('/update', passport.checkBuilder,projectController.update); //redirected to controller rendering comments page
-router.get('/community-engagement', passport.checkAuthentication,projectController.community_engagement); //redirected to controller rendering comments page
+router.get('/community-engagement', projectController.community_engagement); //redirected to controller rendering comments page
 router.get('/project-description/:id', passport.checkAuthentication, projectController.project_disc); //redirected to controller rendering comments page
 
 module.exports = router;
